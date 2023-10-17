b7957f339126a3cc277dd14ae709af556d0a543e
shubham11052002 <145835335+shubham11052002@users.noreply.github.com>
2023-10-17 12:38:53 +0530

    Add files via upload

---
+++ firstloop.java
@@ -0,0 +1,10 @@
+public class firstloop {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.println("Hello Java :" + i);
+            i++;
+        } while (i < 5);
+
+    }
+}
---
+++ variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args) {
+        byte age = 22; 
+        byte age2 = (byte) 130;
+        System.out.println(age2);
+        float percent = (float) 10.5;
+
+    }
+}
